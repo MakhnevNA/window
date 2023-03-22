1235d85e083936f687e1797ceb6ada89f9afd907
Nikita <nikitka.makhnev@mail.ru>
2023-03-22 11:17:46 +0500

    fixed bags

--- dist/script.js
+++ dist/script.js
@@ -15568,24 +15568,14 @@ function modals() {
       document.querySelector(selector).style.display = 'block';
       document.body.style.overflow = 'hidden';
     }, time);
-  } // function calcScroll() {
-  // 	let div = document.createElement('div');
-  // 	div.style.width = "50px";
-  // 	div.style.height = "50px";
-  // 	div.style.overflowY = 'scroll';
-  // 	div.style.visibility = 'hidden';
-  // 	document.body.appendChild(div);
-  // 	let scrollWidth = div.offsetWidth - div.clientWidth
-  // 	div.remove();
-  // 	return scrollWidth;
-  // }
-
+  }
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
   bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close');
-  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false); // showModalByTime('.popup', 60000);
+  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+  showModalByTime('.popup', 60000);
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (modals);
